59c18b6ad4a49d96f750b69be81f828a662dc939
Jonathan Guiang <jkguiang@gmail.com>
2022-03-07 13:04:18 -0800

    added scripts for cleaning up

---
+++ cleanup/cleanup.py
@@ -0,0 +1,56 @@
+import argparse
+import glob
+import os
+from subprocess import Popen, PIPE
+from itertools import islice
+from haddnano import haddNano
+
+class MessyDir:
+    def __init__(self, path, min_chunk_size):
+        self.path = path
+        self.root_files = glob.glob(f"{path}/*.root")
+        # Get directory size in bytes
+        du_cmd = Popen(f"du -bs {path}".split(), stdout=PIPE, stderr=PIPE)
+        resp = du_cmd.communicate()
+        self.size = int(resp[0].decode("utf-8").split("\t")[0])
+        # Get number of files for each chunk (may be uneven)
+        n_files_total = len(self.root_files)
+        n_chunks = int(self.size//min_chunk_size)
+        chunk_n_files = []
+        if n_chunks > 0 and n_chunks < n_files_total:
+            chunk_n_files = [n_files_total//n_chunks for _ in range(n_chunks)]
+            for chunk_i in range(n_files_total%n_chunks):
+                chunk_n_files[chunk_i] += 1
+        else:
+            chunk_n_files = [n_files_total]
+        # Organize ROOT files in directory into chunks
+        iterator = iter(self.root_files)
+        self.chunks = [list(islice(iterator, 0, n_files)) for n_files in chunk_n_files]
+
+    def clean(self):
+        for chunk_i, chunk in enumerate(self.chunks):
+            haddNano(f"{self.path}/merged_{chunk_i}.root", chunk)
+            for root_file in chunk:
+                os.remove(root_file)
+
+def cleanup(basedir, min_chunk_size=20*10**6):
+    if min_chunk_size < 10*10**6:
+        print("ERROR: minimum chunk size less than 10 MB")
+        return
+    for path in glob.glob(f"{basedir}/*"):
+        if os.path.isdir(path):
+            messydir = MessyDir(path, min_chunk_size)
+            messydir.clean()
+
+if __name__ == "__main__":
+    cli = argparse.ArgumentParser(description="Merge ROOT files in messy subdirs")
+    cli.add_argument(
+        "basedir", type=str, nargs=1,
+        help="base dir of subdirs to clean up"
+    )
+    cli.add_argument(
+        "-c", "--chunksize", type=int, default=20*10**6,
+        help="minimum chunk size in bytes (default 20 MB; must be > 10 MB)"
+    )
+    args = cli.parse_args()
+    cleanup(args.basedir, min_chunk_size=args.chunksize)
